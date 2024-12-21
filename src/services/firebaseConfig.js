afb53c3860795834c4522c93bd000897d6e53902
Ayub Islam <ayubislam863@gmail.com>
2024-12-22 02:40:54 +0600

    scure the firebase key

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCQaEHozZ8n57kR_GsSpBJYva2_hwigZXo",
-    authDomain: "job-assignment-619c8.firebaseapp.com",
-    projectId: "job-assignment-619c8",
-    storageBucket: "job-assignment-619c8.firebasestorage.app",
-    messagingSenderId: "232182970771",
-    appId: "1:232182970771:web:6469b902818830be3a9066",
-    measurementId: "G-DYHJBZK24Q"
-  };
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+};
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
